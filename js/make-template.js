c9ebe93193de0ae5cac67788ade8f08848551906
DimitrisBourgioitis <dbourgiotis@convertgroup.com>
2017-09-03 18:50:20 +0300

    init

---
+++ js/make-template.js
@@ -0,0 +1,70 @@
+let coordinates = [];
+let coordinatesWithTime = [];
+let t = 0;
+let recursion;
+let chart;
+
+let table = document.getElementById('template-table')
+// table.addEventListener('mouseenter', setTemplate);
+table.addEventListener('mousemove', setTemplate);
+table.addEventListener('mouseleave', clearCoordinates);
+
+
+function setTemplate(event){
+    console.log(event)
+    let x = event.clientX;
+    let y = event.clientY;
+    coordinates.push([x,y,t])
+    if(!recursion){
+        setTime(null)
+    }
+}
+
+function setTime(event){
+    let x = coordinates[coordinates.length-1][0]
+    let y = coordinates[coordinates.length-1][1]
+    coordinatesWithTime.push([x,y,t])
+    recursion = setInterval( recur => {
+        t += 10;
+        x = coordinates[coordinates.length-1][0]
+        y = coordinates[coordinates.length-1][1]
+        coordinatesWithTime.push([x,y,t])
+    },100)
+}
+
+function clearCoordinates(event){
+    if(chart){
+        chart.destroy();
+        chart = null
+    }
+    t = 0;
+    clearInterval(recursion)
+    console.log(coordinatesWithTime)
+    chart = generateChart()
+    // coordinates.length = []
+    // coordinatesWithTime.length = []
+}
+
+function generateChart(){
+    let y =['y'];
+    let x = ['x'];
+    let t = ['t'];
+    for( let pair of coordinatesWithTime){
+        x.push(pair[0])
+        y.push(pair[1])
+        t.push(pair[2])
+    }
+    chart = c3.generate({
+        bindto: '#chart',
+        data: {
+            xs: {
+                x: 't',
+            },
+            columns: [
+                t,
+                x,
+            ],
+            type: 'scatter'
+        }
+    });
+}
